ae018b061962b07c7390f8f4ea68e219cf62da20
Maxwell Wang <maxwang@udel.edu>
2022-04-07 13:51:27 -0400

    Finished all basic math operations

--- src/Basic Math/EasyMath.java
+++ src/Basic Math/EasyMath.java
@@ -8,4 +8,21 @@ public class EasyMath {
     public int add(int number, int number2){
         return number + number2;
     }
+
+    public int subtract(int number, int number2){
+        return number1 - number2;
+    }
+
+    public int mutliply(int number, int number2){
+        return number1 * number2;
+    }
+
+    publit int divide(int number, int number2){
+        if(number2 != 0){
+            return number / number2;
+        }
+        else{
+            return null;
+        }
+    }
 }
