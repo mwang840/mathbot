cb24a77ce9aede1a50308b26e3ec24151db221fd
Maxwell Wang <maxwang@udel.edu>
2022-04-11 14:20:57 -0400

    added sine and cosine functions

--- src/Trignometry.java
+++ src/Trignometry.java
@@ -3,4 +3,14 @@ import java.util.Scanner;
 
 public class Trignometry{
     private double number;
+
+    public double sin(double number){
+        double radians = Math.toRadians(number);
+        return Math.Sin(radians);
+    }
+
+    public double cosine(double number){
+        double radians = Math.toRadians(number);
+        return Math.cos(radians);
+    }
 }
