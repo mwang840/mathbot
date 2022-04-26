33b5b17ac2865b97dc57093e885dffc5f70d61fa
Maxwell Wang <maxwang@udel.edu>
2022-04-26 09:50:31 -0400

    Added some basic client commands and further modified compile statements

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,16 @@
-import discord;
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.AccountType;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import reactor.core.publisher.Mono;
 import java.lang.Math;
 import java.util.Scanner;
 
 public static void main (string[] args){
-    int num =2;
-
-    System.out.println(root(num));
+        DiscordClient client = DiscordClient.create("TOKEN");
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+        gateway.on(ReadyEvent.class, event ->
+        Mono.fromRunnable(() -> {
+            final User self = event.getSelf();
+        System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+        })));
+        login.block();
 }
